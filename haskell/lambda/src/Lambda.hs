00d4062fc31e3a086572d893261fde8292b3e3af
Scott Fleischman <scott.fleischman@logos.com>
2015-12-08 12:52:34 -0800

    Add examples

--- haskell/lambda/src/Lambda.hs
+++ haskell/lambda/src/Lambda.hs
@@ -3,11 +3,11 @@ module Lambda where
 data Term v
   = Var v
   | Lam v (Term v)
-  | App (Term v) (Term v)
+  | Ap (Term v) (Term v)
   deriving (Eq, Ord, Show, Read)
 
 getRedexes :: Term v -> [Term v]
-getRedexes t@(App t1@(Lam _ _) t2) = t : getRedexes t1 ++ getRedexes t2
-getRedexes (App t1 t2) = getRedexes t1 ++ getRedexes t2
+getRedexes t@(Ap t1@(Lam _ _) t2) = t : getRedexes t1 ++ getRedexes t2
+getRedexes (Ap t1 t2) = getRedexes t1 ++ getRedexes t2
 getRedexes (Lam _ t) = getRedexes t
 getRedexes (Var _) = []
